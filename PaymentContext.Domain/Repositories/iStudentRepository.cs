f0432923302202f4bd7de7f96c8aac5d873375d3
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-01-11 18:07:22 -0300

    teste das queries

--- PaymentContext.Domain/Repositories/iStudentRepository.cs
+++ PaymentContext.Domain/Repositories/iStudentRepository.cs
@@ -2,8 +2,9 @@ using PaymentContext.Domain.Entities;
 
 namespace PaymentContext.Domain.Repositories
 {
+    //Interface para o repositório de estudantes
     public interface IStudentRepository
-    {
+    {   
         bool DocumentExists(string document);
         bool EmailExists(string email);
         void CreateSubscription(Student student);
